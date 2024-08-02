93408e716bc3b9e22d4626b489fbaa13f34a9f90
J <cnbjjj@outlook.com>
2024-08-02 00:02:33 -0500

    Fix deploy migration problem

---
+++ ProductManagement.DAL/ProductManagementContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace ProductManagement.DAL
+{
+    public class ProductManagementContextFactory : IDesignTimeDbContextFactory<ProductManagementContext>
+    {
+        public ProductManagementContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables()
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            var optionsBuilder = new DbContextOptionsBuilder<ProductManagementContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new ProductManagementContext(optionsBuilder.Options);
+        }
+    }
+}
